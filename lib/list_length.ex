f10d41f7e777109ee80744190f3cea2ccd3eb40f
Danilo Katayama <danilokatayama@gmail.com>
2022-02-09 13:52:09 -0300

    challenge completed

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+
+  def count([], acc), do: acc
+
+  def count([_head | tail], acc) do
+    acc = acc + 1
+    count(tail, acc)
   end
 end
